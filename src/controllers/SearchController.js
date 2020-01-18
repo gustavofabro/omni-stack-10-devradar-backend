86a06409345b646380e10c111259bf0c0a5376ca
gustavofabro <gustavofabro.f@gmail.com>
2020-01-18 13:05:55 -0300

    comunicação web socket  para alerta de novos devs

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -21,8 +21,6 @@ module.exports = {
       }
     })
 
-    console.log('>' + devs)
-
     return res.json({ devs })
   },
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./routes')
 const cors = require('cors')
+const http = require('http')
+const routes = require('./routes')
+
+const { setupWebsocket } = require('./websocket')
+
 const app = express()
+const server = http.Server(app)
+
+setupWebsocket(server)
 
 mongoose.Promise = global.Promise
-
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ue2yh.mongodb.net/semana10?retryWrites=true&w=majority', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
@@ -15,4 +21,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+server.listen(3333)
