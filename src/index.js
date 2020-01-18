4decb66503a6174cedd43c91f2e7c748a1cba447
gustavofabro <gustavofabro.f@gmail.com>
2020-01-18 09:40:13 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 const app = express()
+
+mongoose.Promise = global.Promise
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ue2yh.mongodb.net/semana10?retryWrites=true&w=majority', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
